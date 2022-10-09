b4955483d75b40d7696c8f8ec7db652245eb496e
6019thinkingman <99737495+6019thinkingman@users.noreply.github.com>
2022-10-10 00:59:48 +0300

    updated

--- stock/signals.py
+++ stock/signals.py
@@ -1,8 +1,20 @@
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
-from .models import Transaction
+from .models import Transaction, Product
 
 @receiver(pre_save, sender=Transaction)
 def calculate_total_price(sender, instance, **kwargs):
     if not instance.price_total:
         instance.price_total = instance.quantity * instance.price
+        
+@receiver(pre_save, sender=Transaction)       
+def update_stock(sender, instance, **kwargs):
+    product = Product.object.get(id=instance.product_id)
+    if instance.transaction == 1:
+        if not product.stock:
+            product.stock = instance.quantity
+        else: product.stock += instance.quantity
+    else:
+        product.stock -= instance.quantity
+        
+    product.save()
--- stock/views.py
+++ stock/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets, filters
+from .models import(
+    Category,
+    Brand,
+    Product,
+    Firm,
+    Transaction
+)
+
+from.serilaizers import (
+    CategorySerializer,
+    BrandSerializer
+)
+
+class CategoryView(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    filter_backends =[filters.SearchFilter]
+    search_fields = ["name"]
+    
+class BrandView(viewsets.ModelViewSet):
+    queryset = Brand.objects.all()
+    serializer_class = BrandSerializer
+    filter_backends =[filters.SearchFilter]
+    search_fields = ["name"]
