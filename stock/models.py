58f9151ca556cec12b978b3821b1a76fbe6fbb05
6019thinkingman <99737495+6019thinkingman@users.noreply.github.com>
2022-10-09 20:15:53 +0300

    models updated

--- stock/models.py
+++ stock/models.py
@@ -1,7 +1,7 @@
-from unicodedata import category
-from unittest.util import _MAX_LENGTH
-from django.db import models
 
+from unicodedata import decimal
+from django.db import models
+from django.contrib.auth.models import User
 # Create your models here.
 class UpdateCreate(models.Model):
     created = models.DateTimeField(auto_now_add=True)
@@ -19,8 +19,41 @@ class Category(models.Model):
 class Brand(models.Model):
     name = models.CharField(max_length=30)
     
+    def __str__(self):
+        return self.name
+    
 class Product(UpdateCreate):
     name = models.CharField(max_length=100)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE, releted_name="b_products")
     stock = models.SmallIntegerField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.name
+    
+class Firm(UpdateCreate):
+    name = models.CharField(max_length=30)
+    phone = models.CharField(max_length=15)
+    address = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
+    
+class Transaction(UpdateCreate):
+    TRANSACTION = (
+        (1, "IN"),
+        (0, "OUT")
+    )
+    
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    firm = models.ForeignKey(Firm, on_delete=models.SET_NULL, null=True, related_name="transactions")
+    transaction = models.SmallIntegerField(vhoices=TRANSACTION)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="transaction")
+    quantity = models.SmallIntegerField()
+    price = models.DecimalField(max_digit=6, decimal_places=2)
+    price_total = models.DecimalField(max_digit=8, decimal_places=2)
+    
+    def __str__(self):
+        return f"{self.transaction}-{self.product}-{self.quantity}"
+    
+    
